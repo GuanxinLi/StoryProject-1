96391934c5ea06da3dbbaf50c0dfc95921dd7613
lee-tammy <tammylee12144@gmail.com>
2018-02-25 08:11:15 -0800

    Add tester file

---
+++ TestTextInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Input;
+
+namespace AzureSuiteForUnity.CognitiveServices.BingSpeech
+{
+	public class TestTextInput : MonoBehaviour {
+		// Use this for initialization
+		public SpeechAPICall call;
+
+		void Start () {
+			//Debug.Log(UnityEngine.Input.GetJoystickNames ());
+		}
+
+	
+		// Update is called once per frame
+		void Update () {
+			if(Input.GetKeyDown (KeyCode.Space)) {
+				Debug.Log ("Pressed");
+				BingSpeechAPIManager.Instance.RecognizeClip();
+			}
+
+		}
+	}
+}
